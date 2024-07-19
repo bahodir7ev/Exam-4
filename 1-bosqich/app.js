fd3cd37149c8b49bf428f0d7c2bddbbc73b6cf9e
Админ <macbahodir@gmail.com>
2024-07-19 16:22:38 +0500

    My exam

---
+++ 1-bosqich/app.js
@@ -0,0 +1,39 @@
+// 1-masala
+
+// let n = 5;
+// let m = 8;
+
+// let count = 0;
+
+// function ordaklarSoni() {
+//     if(m % 2 === 0) {
+//         count = n + (m / 2);
+//         console.log(`O'rdaklar soni: ${count}`);
+//     } else {
+//         console.log(-1);
+//     }
+// }
+
+// ordaklarSoni();
+
+
+// 2-masala
+
+// let randomChipta = 123321;
+// let randomChiptaString = String(randomChipta);
+// let birinchiUchlik = Number(randomChiptaString[0]) + Number(randomChiptaString[1]) + Number(randomChiptaString[2]);
+// let ikkinchiUchlik = Number(randomChiptaString[3]) + Number(randomChiptaString[4]) + Number(randomChiptaString[5]);
+
+// function omadliChipta() {
+//     if(birinchiUchlik == ikkinchiUchlik) {
+//         console.log("YES");
+//     }  else {
+//         console.log("NO");
+//     }
+// }
+
+// omadliChipta(123321)
+
+// console.log(randomChipta);
+// console.log(birinchiUchlik);
+// console.log(ikkinchiUchlik);
